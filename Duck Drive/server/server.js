229c7d1c041ef501b712e581948b28dbd2702ae1
Alaa Alsous <lawyeralaa@gmail.com>
2026-01-10 16:16:01 +0100

    Add loadFiles function

--- Duck Drive/server/server.js
+++ Duck Drive/server/server.js
@@ -2,15 +2,26 @@
 
 import express from "express";
 import fs from "fs";
+
 const app = express();
 const port = 80;
 
-app.use(express.static("public"));
+app.use(express.static("."));
 app.use(express.json());
 
 app.get("/index.html", (req, res) => {
   res.redirect("/");
 });
+
+async function loadFiles() {
+  try {
+    const data = await fs.promises.readdir("./server/files");
+    return data;
+  } catch (error) {
+    console.error("Error loading files:", error);
+    return [];
+  }
+}
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
